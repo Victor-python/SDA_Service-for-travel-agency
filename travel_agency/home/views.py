9bc893532376ccf8f8024552de75fe49e4e6e69a
Victor-python <64462520+Victor-python@users.noreply.github.com>
2020-06-23 11:40:47 +0300

    home & about
    the readme shows in the about page

--- travel_agency/home/views.py
+++ travel_agency/home/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+readme = ''
+
+# copy text from readme
+with open(r'C:\Users\victo\SDA_Service-for-travel-agency\README.md', 'r') as text:
+    readme += text.read()
+
 
 # Create your views here.
+def home(request):
+    # create the home page html to be returned here
+    return HttpResponse('<h1> home page </h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>Page About</h1>'
+                        f'<body> {readme} </body>')
